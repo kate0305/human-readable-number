997d6a1699edc330a12e82cdcb7fe19eeec0c422
Katsiaryna Voronich <katarinka403@gmail.com>
2021-08-17 01:39:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
+  const arr = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  const arrDozens = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
   
+  let arrNumbers = [];
+  
+  const numberConvert = () => {
+    let str = number.toString();
+    for (let i = 0; i < str.length; i++) {
+    arrNumbers.push(str[i]);
+  }
+    arrNumbers.map(Number);
+    return arrNumbers;
+  }
+
+  if (number === 0) {return 'zero'}
+
+  if (number > 0 && number < 20) {  
+    return word = arr[number];
+  }
+
+  if (number >= 20 && number < 100) {
+    numberConvert(number);
+    return word = `${arrDozens[arrNumbers[0]]} ${arr[arrNumbers[1]]}`.trim();
+  }
+
+  if (number >= 100) {
+    numberConvert(number);
+    if (arrNumbers[1] < 2) {
+    return word = `${arr[arrNumbers[0]]} hundred ${arr[number % 100]}`.trim();
+    } else {
+    return word = `${arr[arrNumbers[0]]} hundred ${arrDozens[arrNumbers[1]]} ${arr[arrNumbers[2]]}`.split(' ').filter(s => s).join(' ');
+    }
+  }
 }
